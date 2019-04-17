d23df88ab871f83865a1f8d2670e0f2e75008147
Charanjit Kandola <kkandola22@hotmail.com>
2019-04-16 17:06:41 -0700

    finished

---
+++ function-call.js
@@ -0,0 +1,43 @@
+/* # Task
+
+    Write a function that allows you to use Array.prototype.slice without using slice.call or slice.apply to invoke it.
+
+    Normally you have to use slice with call or apply:
+
+        var slice = Array.prototype.slice
+
+        function() {
+        var args = slice.call(arguments) // this works
+        }
+
+    We want this to work:
+
+        var slice = yourFunction
+
+        function() {
+        var args = slice(arguments) // this works
+        }
+
+    ## Conditions
+
+    * Do not use any for/while loops or Array#forEach.
+    * Do not use the `function` keyword :D
+*/
+
+module.exports = Function.call.bind(Array.prototype.slice);
+
+/* Official Solution and Explanation
+    // Explained:
+    // The value of `this` in Function.call is the function
+    // that will be executed.
+    //
+    // Bind returns a new function with the value of `this` fixed
+    // to whatever was passed as its first argument.
+    //
+    // Every function 'inherits' from Function.prototype,
+    // thus every function, including call, apply and bind
+    // have the methods call apply and bind.
+    //
+    // Function.prototype.call === Function.call
+    module.exports = Function.call.bind(Array.prototype.slice)
+*/
